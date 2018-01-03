30707224b2b0fb236bef6dd4da6d68f2ab4abcd5
Anthony Cerruti <tsunderebug@tsunderebug.com>
2018-01-03 15:24:20 -0800

    Correct naming works now

--- src/test/scala/com/oof/boof/SomethingOofedInTheNeighborhood.scala
+++ src/test/scala/com/oof/boof/SomethingOofedInTheNeighborhood.scala
@@ -2,17 +2,13 @@ package com.oof.boof
 
 import github.discordscala.core.Client
 import github.discordscala.core.event.Sharding
-import spire.math.ULong
 
 object SomethingOofedInTheNeighborhood {
 
   def main(args: Array[String]): Unit = {
     implicit val sharding: Sharding = Sharding(1)
-    val c = Client("Bot MzMwOTExNTkxOTIxMDkwNTYw.DSyWcw.xAeV2BbH7Np80RhZkyn2NbeTng8", myShards = Set(0))
+    val c = Client("Bot [token]", myShards = Set(0))
     c.login()
-    println(c.guild(ULong(390751088829005826l)))
-    println(c.guild(ULong(390751088829005826l)))
-    c.logout()
   }
 
 }
