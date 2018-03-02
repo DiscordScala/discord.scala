c5a64393b271a02984370598ba4d8275389ca5ca
Gerd Sattler <gerd@memework.org>
2018-03-02 14:03:56 +0800

    Order is important.

--- src/test/scala/github/discordscala/core/models/PermissionTest.scala
+++ src/test/scala/github/discordscala/core/models/PermissionTest.scala
@@ -8,17 +8,17 @@ class PermissionTest extends FlatSpec with Matchers {
     permission.toLong should be (2146950399l)
   }
 
+  it should "deserialize from long" in {
+    val permissions = Permissions(0x00220000)
+    permissions should be (Permissions(Permission.Speak, Permission.MentionEveryone))
+  }
+
   it should "be queryable" in {
     val permission = Permissions(Permission.ManageEmoji, Permission.Speak)
     permission.has(Permission.ManageEmoji) should be (true)
     permission.has(0x00200000) should be (true)
     permission.has(0x00020000) should be (false)
     permission.has(Permission.UseExternalEmojis) should be (false)
-  }
-
-  it should "deserialize from long" in {
-    val permissions = Permissions(0x00220000)
-    permissions should be (Permissions(Permission.Speak, Permission.MentionEveryone))
   }
 
   it should "add correctly" in {
